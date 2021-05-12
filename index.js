f931743e2bf3a07fa9fb6418de36b6e8500a36c1
Vivek S (Vicky) <vivek.yes002@gmail.com>
2021-05-12 18:40:31 +0530

    Authorizations has been implemented on post.

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const { authRoutes, commentRoutes, likeRoutes, postRoutes, userRoutes } = requir
 
 const app = express()
 const PORT = process.env.PORT || 4000
+const isProd = () => {
+    if (process.env.isProd == 'true')
+        return 'mongodb+srv://vivek_s:WbmDB66k@cluster0.onedp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    else
+        return 'mongodb://localhost:27017/udb'
+}
 
 app.use(bodyParser.json())
 app.use(Cors())
@@ -18,8 +24,7 @@ app.use('/udb/api/v1/like', likeRoutes)
 app.use('/udb/api/v1/post', postRoutes)
 app.use('/udb/api/v1/user', userRoutes)
 
-// mongodb://localhost:27017/udb
-mongoose.connect('mongodb+srv://vivek_s:WbmDB66k@cluster0.onedp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(isProd(), { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Successfully connected to mongodb!!')
         app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
